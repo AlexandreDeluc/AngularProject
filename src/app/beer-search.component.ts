db89adf61cec0765ae336205d1d64777e7136d11
Alexandre Deluc <alexandre.deluc07@gmail.com>
2022-01-05 18:53:46 +0100

    css

--- src/app/beer-search.component.ts
+++ src/app/beer-search.component.ts
@@ -7,11 +7,14 @@ import { debounceTime, distinctUntilChanged, map } from "rxjs/operators";
   selector: 'beer-beer-search',
   template: `
     <div>
-      <label for="beer-search">Recherche</label>
+      <label id="search" for="beer-search">Rechercher :</label>
       <input id="beer-search" [formControl]="searchInputCtrl">
     </div>
   `,
   styles: [
+    'div {text-align:center; width: auto; padding:30px;background-color:#f0ecda; border-radius: 10px;;}',
+    '#search {font-size: 25px; font-family: sans-serif;}',
+    '#beer-search {margin-left: 10px; height: 20px;}'
   ]
 })
 export class BeerSearchComponent implements OnInit {
