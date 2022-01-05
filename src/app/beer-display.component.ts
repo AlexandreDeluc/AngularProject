ef9c2f3424c65075207fc1d599de6d99e3612a78
Alexandre Deluc <alexandre.deluc07@gmail.com>
2022-01-05 18:40:24 +0100

    Merge branch 'main' of https://github.com/AlexandreDeluc/AngularProject into main

--- src/app/beer-display.component.ts
+++ src/app/beer-display.component.ts
@@ -4,15 +4,24 @@ import { Beer } from './beer.model';
 @Component({
   selector: 'beer-beer-display',
   template: `
-    <h2>{{ beer.id }} {{ beer.name }}</h2>
+  <div id="block">
+    <h2>{{ beer.id }}. {{ beer.name }}</h2>
     <p>{{ beer.tagline }}</p>
+    <div id="biere">
       <img src="{{ beer.image_url }}" alt="">
-    <p>Since {{ beer.first_brewed }}</p>
-    <button [routerLink]="['/contacts', beer.id, 'edit']">Details</button>
+      <p>Since {{ beer.first_brewed }}</p>
+      <button [routerLink]="['/contacts', beer.id, 'edit']">Details</button>
+    </div>
+  </div>
 
 
   `,
   styles: [
+    '*{font-family: sans-serif;}',
+    '#block {flex-direction: column; margin-left: 30px; margin-bottom: 45px;}',
+    '#biere {text-align: center;}',
+    '#biere img {width: 100px; height: 300px; justify-content: center;}',
+    '#biere button {width: 100px;} ',
   ]
 })
 export class BeerDisplayComponent implements OnInit {
